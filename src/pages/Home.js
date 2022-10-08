c71694c634efe23c86a5b6dda447742bdf4bf72b
ShivangKacha <shivangkachasbk9@gmail.com>
2022-10-08 20:19:28 +0530

    game e

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,51 @@
-// import './App.css';
-import '../../src/style/App.css';
-import { Link } from 'react-router-dom';
-import {useState} from 'react';
+import "../../src/style/App.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 function Home() {
-  let [query , setQuery] = useState('');
+  let [query, setQuery] = useState("");
   return (
-     <>
     <div className="App">
-            </div>
+      <div
+        id="intro"
+        className="bg-image bg-opacity-25 bg-dark shadow-2-strong"
+      >
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col"></div>
+          </div>
 
-
-    <div id="intro" className="bg-image shadow-2-strong">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col"></div>
-        </div>
-
-        <div className="row">
-          <form
-            className="col-6 mx-auto position-absolute top-50 start-50 translate-middle"
-            role="search"
-          >
-
-            <div className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="Search for topics here" aria-label="Seacrh News" aria-describedby="button-addon" onChange={(e) => {setQuery(e.target.value)}} value={query} />
-              <Link className="btn btn-warning" type="button" id="button-addon" to={`/dashboard/${query}`}> Go </Link>
-            </div>
-          </form>
+          <div className="container d-flex flex-column justify-content-center vh-100 gy-5">
+            <h1 className="display-1 mb-2 shadow text-center text-white">
+              AALEKH
+            </h1>
+            <form className="col-6 mx-auto" role="search">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Recipient's username"
+                  aria-label="Seacrh News"
+                  aria-describedby="button-addon"
+                  onChange={(e) => {
+                    setQuery(e.target.value);
+                  }}
+                  value={query}
+                />
+                <Link
+                  className="btn btn-primary"
+                  type="button"
+                  id="button-addon"
+                  to={`/dashboard/${query}`}
+                >
+                  {" "}
+                  Go{" "}
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-
     </div>
-    </>
   );
-
 }
 export default Home;
